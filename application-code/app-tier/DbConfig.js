c0adc7858a5f4dbf989d32d02bd87c1c7191f595
veerajdevops <145025841+veerajdevops@users.noreply.github.com>
2023-09-22 18:15:55 +0530

    Update DbConfig.js
    added , host,user etc

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'threetier-database.cluster-caa19tnopw2g.eu-north-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '9008881772',
+    DB_DATABASE : 'webappdb'
 });
